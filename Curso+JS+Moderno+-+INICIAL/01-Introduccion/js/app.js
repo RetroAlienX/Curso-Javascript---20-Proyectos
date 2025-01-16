28c9dabe772d249c8e204a1be915991bc25ec007
Ghost <ghxst023@gmail.com>
2025-01-15 20:33:45 -0600

    Initializing repository and starting learning

---
+++ Curso+JS+Moderno+-+INICIAL/01-Introduccion/js/app.js
@@ -0,0 +1,9 @@
+/*const nombre = prompt("Cual es tu nombre?");
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo Javascript Moderno`*/
+
+// const nombre = prompt("Cual es tu nombre?");
+// document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo Javascript Moderno`
+
+//* Toma el nombre y lo muestra en pantalla
+const nombre = prompt("Cual es tu nombre?");
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo Javascript Moderno`

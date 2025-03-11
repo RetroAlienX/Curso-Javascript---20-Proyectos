04d8c5c446eecbcef05cbf38c2e99538af9649a8
Ghost <ghxst023@gmail.com>
2025-03-10 19:50:43 -0600

    validando email, regex,  alertas de validacion

--- Curso+JS+Moderno+-+INICIAL/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso+JS+Moderno+-+INICIAL/16-PROYECTO-EnviarEmail/js/app.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
   inputMensaje.addEventListener("blur", validar);
 
   function validar(e) {
+    // console.log(e.target.parentElement);
     // console.log(e.target.id);
     // console.log("Desde la funcion de validar");
     // console.log(e.target.value);
@@ -38,14 +39,27 @@ document.addEventListener("DOMContentLoaded", function () {
     if (e.target.value.trim() === "") {
       // console.log("Esta vacio");
       //! Validando cada campo y agregando el error correcto basado en el id de cada elemento HTML
-      mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
-    } else {
-      console.log("Si hay algo");
+      mostrarAlerta(
+        `El campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
+      return;
     }
+
+    if (e.target.id === "email" && !validarEmail(e.target.value)) {
+      mostrarAlerta("El email no es valido", e.target.parentElement);
+      return;
+    }
+
+    limpiarAlerta(e.target.parentElement);
+    // console.log("despues del IF");
   }
 
   //! Mostrar alerta si el input esta vacio o es mandatorio
-  function mostrarAlerta(mensaje) {
+  function mostrarAlerta(mensaje, referencia) {
+    //! Comprueba si ya existe una alerta
+    limpiarAlerta(referencia);
+
     // console.log("Hubo un error");
     //! Generar una alerta con scripting en HTML
     const error = document.createElement("P");
@@ -54,7 +68,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // console.log(error);
     //! Añadiendo la alerta en HTML (inyectar el error al formulario)
-    formulario.appendChild(error);
+    //! Mostrando alerta junto a su campo
+    // formulario.appendChild(error);
+    referencia.appendChild(error);
+  }
+
+  //! Ocultar alertas si pasa la validacion, limpiando alertas
+  function limpiarAlerta(referencia) {
+    //! Comprueba si ya existe una alerta
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove();
+    }
+    console.log("Desde limpiar alerta");
+  }
+
+  function validarEmail(email) {
+    const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    const resultado = regex.test(email);
+    // console.log(resultado);
+    return resultado;
   }
 });
 

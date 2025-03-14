1df9fdad5b546aa3a6b53fae4f954354bdc8823f
Ghost <ghxst023@gmail.com>
2025-03-13 20:34:28 -0600

    finalizando simulador de envio de emails

--- Curso+JS+Moderno+-+INICIAL/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso+JS+Moderno+-+INICIAL/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,12 +1,23 @@
 //! Primeros pasos con el proyecto
 
 document.addEventListener("DOMContentLoaded", function () {
+  //! Creando el objeto principal para validar y sincronizar datos
+  const email = {
+    email: "",
+    asunto: "",
+    mensaje: "",
+  };
+
+  console.log(email);
+
   //! Seleccionar los elementos de la interfaz
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
   const formulario = document.querySelector("#formulario");
-
+  const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+  const btnReset = document.querySelector('#formulario button[type="reset"]');
+  const spinner = document.querySelector("#spinner");
   //   console.log(inputMensaje);
 
   //! Asignar eventos
@@ -25,9 +36,52 @@ document.addEventListener("DOMContentLoaded", function () {
   //! Asignar eventos
   //! Reemplazando el callback de la funcion de arriba por una funcion creada para reutilizar el codigo y evitar repetitivo, en la funcion validar del addEventListener no se usan parentesis por que si le ponemos, llamara la funcion desde el inicio y se debe llamar cuando se dispare el evento.
 
-  inputEmail.addEventListener("blur", validar);
-  inputAsunto.addEventListener("blur", validar);
-  inputMensaje.addEventListener("blur", validar);
+  inputEmail.addEventListener("input", validar);
+  inputAsunto.addEventListener("input", validar);
+  inputMensaje.addEventListener("input", validar);
+
+  formulario.addEventListener("submit", enviarEmail);
+
+  btnReset.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    //!reiniciar el objeto
+    resetFormulario();
+  });
+
+  //! Añadiendo un spinner
+  function enviarEmail(e) {
+    e.preventDefault();
+
+    spinner.classList.add("flex");
+    spinner.classList.remove("hidden");
+
+    //! Mostrar el spinner cuando se envia el email y reinicar el formulario
+    setTimeout(() => {
+      spinner.classList.remove("flex");
+      spinner.classList.add("hidden");
+      resetFormulario();
+      //!Crear una alerta
+      const alertaExito = document.createElement("P");
+      alertaExito.classList.add(
+        "bg-green-500",
+        "text-white",
+        "p-2",
+        "text-center",
+        "rounded-lg",
+        "mt-10",
+        "font-bold",
+        "text-sm",
+        "uppercase"
+      );
+      alertaExito.textContent = "Mensaje enviado correctamente";
+
+      formulario.appendChild(alertaExito);
+      setTimeout(() => {
+        alertaExito.remove();
+      }, 3000);
+    }, 3000);
+  }
 
   function validar(e) {
     // console.log(e.target.parentElement);
@@ -43,16 +97,27 @@ document.addEventListener("DOMContentLoaded", function () {
         `El campo ${e.target.id} es obligatorio`,
         e.target.parentElement
       );
+      email[e.target.name] = "";
+      comprobarEmail();
       return;
     }
 
     if (e.target.id === "email" && !validarEmail(e.target.value)) {
       mostrarAlerta("El email no es valido", e.target.parentElement);
+      email[e.target.name] = "";
+      comprobarEmail();
       return;
     }
 
     limpiarAlerta(e.target.parentElement);
     // console.log("despues del IF");
+
+    //! Asignar los valores
+    email[e.target.name] = e.target.value.trim().toLowerCase();
+    // console.log(email);
+
+    //! Comprobar el objeto de email
+    comprobarEmail();
   }
 
   //! Mostrar alerta si el input esta vacio o es mandatorio
@@ -89,8 +154,26 @@ document.addEventListener("DOMContentLoaded", function () {
     // console.log(resultado);
     return resultado;
   }
-});
 
-//! Continuar aqui
+  function comprobarEmail() {
+    // console.log(email);
+    if (Object.values(email).includes("")) {
+      btnSubmit.classList.add("opacity-50");
+      btnSubmit.disabled = true;
+    } else {
+      btnSubmit.classList.remove("opacity-50");
+      btnSubmit.disabled = false;
+    }
+  }
+
+  function resetFormulario() {
+    email.email = "";
+    email.asunto = "";
+    email.mensaje = "";
+
+    formulario.reset();
+    comprobarEmail();
+  }
+});
 
 //! TODO: Codigo limpio se removieron comentarios y console.log innecesarios
